5fd9a3ea34fdc93bbfae212752b75b4641f3ced2
DeeperP <damianjcoria@gmail.com>
2025-07-20 14:15:02 -0300

    First

---
+++ HourTrackerApi/src/main/java/com/example/HourTrackerApi/HourTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.HourTrackerApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HourTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HourTrackerApiApplication.class, args);
+	}
+
+}
---
+++ HourTrackerApi/src/test/java/com/example/HourTrackerApi/HourTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HourTrackerApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HourTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
